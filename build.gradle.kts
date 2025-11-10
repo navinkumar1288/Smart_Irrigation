45115be32505ac8989c25ce04e8f4584527a27bf
navinkumar1288 <89594000+navinkumar1288@users.noreply.github.com>
2025-11-10 09:38:24 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,4 @@
-// Root build.gradle.kts placeholder
+// Minimal root build file. Module build scripts (app/) will apply plugins.
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
